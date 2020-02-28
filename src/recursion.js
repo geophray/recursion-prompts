4b1359d84d239a3d79b057f6ca82a8a47f927655
Jeff Maughan <geophray@gmail.com>
2020-02-27 17:08:30 -0700

    Fix problems 1-3

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,43 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    if(n < 0) {
+        return null;
+    } else if(n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    if(!array || array.length === 0) {
+        return 0;
+    } else {
+        var shorterArr = [...array];
+        shorterArr.pop();
+        return array[array.length-1] + sum(shorterArr);
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+    var sum = 0;
+    if(!array || array.length === 0) {
+        return sum;
+    } 
+    for( let i = 0; i < array.length; i++ ) {
+        if(Array.isArray(array[i])) {
+            sum += arraySum(array[i]);
+        } else {
+            sum += array[i];
+        }
+    }
+    return sum;
 };
 
 // 4. Check if a number is even.
